30a8f1fe40d79fa39a51ea15bd415b79264f9b2c
Sean Downs <spdowns@protonmail.com>
2020-03-26 07:29:03 -0700

    simplefied the statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
-  var age=parseInt(prompt("Hi, how old are you?"));
+  var over21=confirm("Are you over 21?"));
 
-  if (age > 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Now don't go crazy!");
+  if (over21) {
     $('#drinks').show();
   } else {
     $('#under-21').show();
